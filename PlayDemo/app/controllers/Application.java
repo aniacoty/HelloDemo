e9580cb7bb136395e5c23b8786d125d7d75ab748
aniacoty <ania.cat@wp.pl>
2013-05-16 23:30:38 +0200

    jest albo nie

--- PlayDemo/app/controllers/Application.java
+++ PlayDemo/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Jak odpalisz to możesz cieszyć bułę młodociana dziewczynko:)"));
+        return ok(index.render("Byl sobie raz chlopczyk i byl on tak glodny ze zjadl wszystkie polskie litery"));
     }
   
 }
