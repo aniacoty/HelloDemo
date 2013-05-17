ca59c8edfe30672235fa1a7c5be5fd280c17fb46
Apocalyptik <76answer76@gmail.com>
2013-05-17 20:14:35 +0200

    Brudne zabawy w kuchni?

--- PlayDemo/app/controllers/Application.java
+++ PlayDemo/app/controllers/Application.java
@@ -2,13 +2,34 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import play.data.validation.Constraints.*;
+
+import java.util.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-  
+    public static class Kitchen {
+        @Required public String name;
+        public String choice;
+    }
+
     public static Result index() {
-        return ok(index.render("Byl sobie raz chlopczyk i byl on tak glodny ze zjadl wszystkie polskie litery"));
+        return ok(index.render(form(Kitchen.class)));
+    }
+
+    public static Result doIt() {
+        Form<Kitchen> form = form(Kitchen.class).bindFromRequest();
+        if(form.hasErrors()) {
+            return badRequest(index.render(form));
+        } else {
+            Kitchen data = form.get();
+            return ok(
+                    kitchen.render(data.name, data.choice)
+            );
+        }
     }
   
 }
